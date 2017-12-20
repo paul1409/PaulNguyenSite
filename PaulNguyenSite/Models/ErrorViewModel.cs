a7520cf3a7249706d7fd4e1d573f4268f7a0cebc
Paul Nguyen <paul.t.nguyen@sjsu.edu>
2017-12-19 19:42:46 -0800

    Added contact info

--- PaulNguyenSite/Models/ErrorViewModel.cs
+++ PaulNguyenSite/Models/ErrorViewModel.cs
@@ -1,9 +1,7 @@
 using System;
 
-namespace PaulNguyenSite.Models
-{
-    public class ErrorViewModel
-    {
+namespace PaulNguyenSite.Models {
+    public class ErrorViewModel {
         public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
